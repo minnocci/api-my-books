406cff71e740e17e0b949e108a69c4a223d51b19
Juan Minnocci <juan@monoqi.de>
2019-02-26 14:24:07 +0100

    Allow cross domain

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ mongoose.Promise = global.Promise
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
+app.use(allowCrossDomain)
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
