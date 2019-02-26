d39c643826eda9ec21071ef0bb256c3e8a22d24e
Juan Minnocci <juan@monoqi.de>
2019-02-26 13:28:28 +0100

    Implement server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const Auth = require('./api/models/authModel')
+const Me = require('./api/models/meModel')
+const Categories = require('./api/models/categoriesModel')
+const Books = require('./api/models/booksModel')
+  
+const mongoDB = 'mongodb+srv://api-my-books:api-my-books@cluster0-8ew92.mongodb.net/api-my-books?retryWrites=true'
+mongoose.connect(mongoDB, { useNewUrlParser: true })
+mongoose.Promise = global.Promise
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+const authRoutes = require('./api/routes/authRoutes')
+const meRoutes = require('./api/routes/meRoutes')
+const categoriesRoutes = require('./api/routes/categoriesRoutes')
+const booksRoutes = require('./api/routes/booksRoutes')
+authRoutes(app)
+meRoutes(app)
+categoriesRoutes(app)
+booksRoutes(app)
+
+app.listen(port)
+
+console.log('todo list RESTful API server started on: ' + port)
