228178601cc6683f59a4f4ebd15063583bda64bb
Juan Minnocci <juan@monoqi.de>
2019-02-26 14:30:41 +0100

    Configure headers fix

--- api/config/headers.js
+++ api/config/headers.js
@@ -1,4 +1,4 @@
-var allowCrossDomain = function(req, res, next) {
+exports.allowCrossDomain = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
